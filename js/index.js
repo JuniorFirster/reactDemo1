20ed4ccbd18b7e454504d5f0022a7a6c72689114
JuniorFirster <zhangxing114@126.com>
2019-05-12 17:48:28 +0800

    remove console code

--- js/index.js
+++ js/index.js
@@ -3,7 +3,4 @@ import ReactDOM from 'react-dom';
 import ClockComp from './clockComp';
 
 const rootDom = document.getElementById('root-dom');
-// const date = new Date().toLocaleDateString();
-// const time = new Date().toLocaleTimeString();
-// const timestamp = Date.now();
 ReactDOM.render(<ClockComp />, rootDom)
