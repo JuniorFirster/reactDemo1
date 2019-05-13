e1d5e67ce43897b96e0f754dec20b42f2667a057
zhangxinglong001 <zhangxinglong@lianjia.com>
2019-05-13 17:06:59 +0800

    add hash and HtmlWebpackPlugin in hash

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,8 @@
 const webpack = require('webpack');
 const path = require('path');
 const glob = require('glob');
-console.log(path.resolve(__dirname));
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 module.exports = {
     context: path.resolve(__dirname),
     entry: {
@@ -11,17 +12,48 @@ module.exports = {
     mode: 'development',
     module:{
         rules: [
-            {
-              test: /\.jsx?$/,
-              loader: 'babel-loader',
-              query: {compact: false},
-            }
+          {
+            test: /\.jsx?$/,
+            loader: 'babel-loader',
+            query: {compact: false},
+          },
+          {
+            test: /\.html$/,
+            loader: "html-loader",
+          },
         ]
     },
+    optimization: {
+      minimize: true,
+      runtimeChunk: 'single',
+      splitChunks: {
+        chunks: 'all'
+      }
+    },
+    plugins: [
+      new CopyWebpackPlugin([{
+        from: path.join(__dirname, 'favicon.ico'),
+        to: ''
+      }]),
+      new HtmlWebpackPlugin({
+        filename: "index.html",
+        template: path.join(__dirname, 'index.html'),
+        chunks: ["index"], // entry中的app入口才会被打包
+        // minify: {
+        //   // 压缩选项
+        //   collapseWhitespace: true
+        // },
+        // // files: {
+        // //   js: ''
+        // // },
+      }),
+    ],
     output: {
       path: path.resolve(__dirname, 'dist'),
       publicPath: '/',
-      filename: 'build.js'
+      chunkFilename: '[name]-[chunkhash].js',
+      hashDigestLength: 10,
+      filename: '[name]-[contenthash].js'
     },
     resolve: {
       // 省略后缀
