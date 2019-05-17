8dea4e7c3584a559fe4464169633b4a9874c2b0f
zhangxinglong001 <zhangxinglong@lianjia.com>
2019-05-17 11:36:25 +0800

    remove src

--- src/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import ClockComp from './clockComp.js';
-
-const rootDom = document.getElementById('root-dom');
-
-// ReactDOM.render(<ClockComp />, rootDom)
